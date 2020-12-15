5f74e849831ff02217c3b67021f0ddb6bd4e2724
AaronJeffers1996 <47363247+AaronJeffers1996@users.noreply.github.com>
2020-12-15 13:50:37 +0000

    UI Scale fixed. FPS counter added.

---
+++ Assets/DevTools.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DevTools : MonoBehaviour
+{
+    
+    public Text fpsCounterText;    
+	private float fpsCount;
+
+	IEnumerator Start()
+	{
+		while (true)
+		{
+			if (Time.timeScale == 1)
+			{
+				yield return new WaitForSeconds(0.1f);
+				fpsCount = Mathf.RoundToInt(1 / Time.deltaTime);
+			}
+			fpsCounterText.text = "FPS: " + fpsCount;
+			yield return new WaitForSeconds(0.5f);
+		}
+	}
+}
