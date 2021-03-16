fcba6f9b91f561e42ad80205e69628b2a71f53f3
AlanxxonE <AlanxxonE@users.noreply.github.com>
2021-03-16 16:39:38 +0000

    AIPathing&TargetDecisionWalkers

---
+++ Assets/Scripts/ProductionScripts/AI/WorkerAI.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WorkerAI : MonoBehaviour
+{
+	private AIManager AIManagerRef;
+
+	private NavMeshAgent worker; //Nav Mesh Agent reference
+
+	private Transform lastTarget;
+	private Transform currentTarget;
+
+	private bool timeToMove = false;
+
+    private void Start()
+    {
+		AIManagerRef = GetComponentInParent<AIManager>();
+		worker = GetComponent<NavMeshAgent>();
+		currentTarget = AIManagerRef.targetLocations[0];
+		StartCoroutine(TimeToWait());
+	}
+
+    private void Update()
+	{
+		//int randomNo = Random.Range(0, 1); //Instantiate random number generator
+
+		if (Vector3.Distance(this.transform.position, currentTarget.transform.position) < 1 && timeToMove)
+		{
+			timeToMove = false;
+		}
+
+		if(!timeToMove)
+        {
+			Debug.Log("settarget");
+			SetTarget();
+			timeToMove = true;
+		}
+	}
+
+	private void SetTarget()
+    {
+		lastTarget = currentTarget;
+
+		int randomLocation = Random.Range(0, AIManagerRef.targetLocations.Count + 1); //generates a random reference to a member of the target locations list
+
+        while (AIManagerRef.targetLocations[randomLocation] == lastTarget)
+        {
+            randomLocation = Random.Range(0, AIManagerRef.targetLocations.Count);
+        }
+
+        currentTarget = AIManagerRef.targetLocations[randomLocation];
+
+		StartCoroutine(TimeToWait());
+	}
+
+	IEnumerator TimeToWait()
+    {
+		
+		int randomTime = Random.Range(5, 10);
+
+		yield return new WaitForSeconds(randomTime);
+
+		worker.SetDestination(currentTarget.transform.position); //Creates path to target for agent to follow
+	}
+}
---
+++ Assets/Scripts/ProductionScripts/Managers/AIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIManager : MonoBehaviour
+{
+    [HideInInspector] public List<Transform> targetLocations; //Reference to list of target locations
+
+    public GameObject workerPrefab; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int targets = this.transform.childCount;
+
+        for (int numOfTargets = 1; numOfTargets <= targets; numOfTargets++)
+        {
+            targetLocations.Add(GetComponentsInChildren<Transform>()[numOfTargets].transform);
+            CloneWorker(numOfTargets);
+        }
+    }
+
+    private void CloneWorker(int index)
+    {
+        GameObject cloneWalker = Instantiate(workerPrefab);
+        cloneWalker.transform.parent = this.transform;
+        cloneWalker.transform.position = targetLocations[index - 1].position;
+    }
+}
