d111d386f885c44a5e6423be17b38b32704a3051
Matthew Hollingsworth <9062232+Cuddins@users.noreply.github.com>
2021-03-16 00:50:49 +0000

    tutorial functionality

--- Assets/Scripts/ProductionScripts/Managers/DialogueManager.cs
+++ Assets/Scripts/ProductionScripts/Managers/DialogueManager.cs
@@ -6,10 +6,21 @@ using UnityEngine.UI;
 public class DialogueManager : MonoBehaviour
 {
     public GameObject dialogueSystem;
+    public Transform tutMarker;
     public Collider droneCollider;
     Text dialogue;
     int currentAmount = 0;
+    float mouseMovement = 0f;
+
+    Vector3 previousMousePosition;
     bool isRunning = false;
+    bool stop = false;
+
+    bool tutCheck1 = false;
+    bool tutCheck2 = false;
+    bool tutCheck3 = false;
+    bool tutCheck4 = false;
+    bool moveOnCheck = false;
 
     int sentenceNumber = 0;
 
@@ -33,9 +44,63 @@ public class DialogueManager : MonoBehaviour
         StartCoroutine(StartIduction(sentenceNumber));
     }
 
-    public void DisplayParagraph(int paraNum, int amountOfLetters)
+    void Update()
     {
-        if (!isRunning)
+        switch (sentenceNumber)
+        {
+            case 2:
+                tutCheck2 = true;
+                tutCheck3 = true;
+                tutCheck4 = true;
+
+                mouseMovement += Mathf.Abs(Input.GetAxis("Mouse X")) * Time.deltaTime;
+
+                if (mouseMovement > 1){tutCheck1 = true;}
+                break;
+            case 3:
+                if(Input.GetKeyDown(KeyCode.W)){tutCheck1 = true;}
+                if(Input.GetKeyDown(KeyCode.A)){tutCheck2 = true;}
+                if(Input.GetKeyDown(KeyCode.S)){tutCheck3 = true;}
+                if(Input.GetKeyDown(KeyCode.D)){tutCheck4 = true;}
+                break;
+            case 5:
+                tutCheck3 = true;
+                tutCheck4 = true;
+
+                if (Input.GetKeyDown(KeyCode.Space)) { tutCheck1 = true; }
+                if (Input.GetKeyDown(KeyCode.LeftShift)) { tutCheck2 = true; }
+                break;
+            case 6:
+                tutCheck2 = true;
+                tutCheck3 = true;
+                tutCheck4 = true;
+
+                if (Vector3.Distance(tutMarker.position, droneCollider.gameObject.transform.position) < 10){tutCheck1 = true;}
+                break;
+            case 7:
+                tutCheck2 = true;
+                tutCheck3 = true;
+                tutCheck4 = true;
+
+                if (Input.GetKeyDown(KeyCode.C)) { tutCheck1 = true; }
+                break;
+            default:
+                tutCheck2 = true;
+                tutCheck3 = true;
+                tutCheck4 = true;
+
+                if (Input.GetKeyDown(KeyCode.Return)){tutCheck1 = true;}
+                break;
+        }
+
+        if (tutCheck1 && tutCheck2 && tutCheck3 && tutCheck4) {StopSentence();}
+    }
+
+    public void DisplayParagraph(int paraNum, int amountOfLetters, bool selfRun)
+    {
+        isRunning = true;
+
+        if (!stop)
         {
             currentAmount = amountOfLetters;
             dialogue.text = " ";
@@ -44,6 +109,7 @@ public class DialogueManager : MonoBehaviour
         {
             currentAmount = 0;
         }
+
         int lettersToDisplay = 0;
         
         foreach (char c in paragraphs[paraNum])
@@ -56,7 +122,7 @@ public class DialogueManager : MonoBehaviour
             lettersToDisplay++;
         }
 
-        if (dialogue.text.Length <= paragraphs[paraNum].Length && !isRunning)
+        if (dialogue.text.Length <= paragraphs[paraNum].Length && !stop) //&& !isRunning)
         {
             currentAmount++;
             StartCoroutine(LetterDelay(paraNum, currentAmount));
@@ -64,32 +130,46 @@ public class DialogueManager : MonoBehaviour
         else
         {
             isRunning = false;
+            stop = false;
         }
     }
 
     IEnumerator LetterDelay(int paraNum, int amountOfLetters)
     {
-        yield return new WaitForSeconds(0.05f);
-        DisplayParagraph(paraNum, amountOfLetters);
+        
+        if (!stop)
+        {
+            yield return new WaitForSeconds(0.05f);
+            DisplayParagraph(paraNum, amountOfLetters, true);
+        }
     }
 
     public void StopSentence()
     {
-        if (currentAmount != 0)
+        if (dialogue.text.Length > paragraphs[sentenceNumber].Length)
         {
-            isRunning = true;
+            moveOnCheck = true;
         }
     }
 
     IEnumerator StartIduction(int sentNum)
     {
-        DisplayParagraph(sentenceNumber,1);
-        yield return new WaitForSeconds(10f);
+        moveOnCheck = false;
+        DisplayParagraph(sentenceNumber,1, false);
+
+        while(!moveOnCheck)
+        {
+            yield return true;
+        }
 
         if (sentenceNumber < paragraphs.Length - 1)
         {
             sentenceNumber++;
             StartCoroutine(StartIduction(sentenceNumber));
+            tutCheck1 = false;
+            tutCheck2 = false;
+            tutCheck3 = false;
+            tutCheck4 = false;
         }
         else
         {
